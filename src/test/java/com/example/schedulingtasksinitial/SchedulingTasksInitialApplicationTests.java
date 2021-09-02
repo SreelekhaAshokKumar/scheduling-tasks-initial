93e8767677ed71010f246d3fafe7622bbbe36d56
SreelekhaAshokKumar <sreelekhashokumar@gmail.com>
2021-09-02 17:16:11 +0200

    added files

---
+++ src/test/java/com/example/schedulingtasksinitial/SchedulingTasksInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulingtasksinitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTasksInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
